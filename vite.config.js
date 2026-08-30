3de77b21cda9f03fb9b1a8f0aba58b2b83f8215c
sanajlee <sana@Sangahui-MacBookAir.local>
2026-08-30 13:58:08 +0900

    2026.08.30.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
 
 export default defineConfig({
   plugins: [react()],
 
   server: {
     host: "0.0.0.0",
+
+    allowedHosts: [
+      ".trycloudflare.com",
+    ],
 
     proxy: {
       "/api": {
